33a142b1f5fc16621e9afe44bdd0c9db0dfd4d2c
Nina Cooper <nina.cooper2019@yandex.ru>
2025-10-28 08:37:45 +0300

    Задание по теме ""Синтаксис языка" и “Базовые типы данных”" - улучшила вывод

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
         int number = new Scanner(System.in).nextInt();
         System.out.println("Введите второе число:");
         int number2 = new Scanner(System.in).nextInt();
-        System.out.println(number + number2);
-        System.out.println(number - number2);
-        System.out.println(number * number2);
+        System.out.println("сумма: " + (number + number2));
+        System.out.println("разность: " + (number - number2));
+        System.out.println("произведение: " + (number * number2));
         double quotient = (double) number / number2;
-        System.out.println(quotient);
+        System.out.println("частное: " + quotient);
     }
 }
