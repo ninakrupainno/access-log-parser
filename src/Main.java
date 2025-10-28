07e8162794926a00a102912313e18a267246ef7e
Nina Cooper <nina.cooper2019@yandex.ru>
2025-10-28 08:32:32 +0300

    Задание по теме ""Синтаксис языка" и “Базовые типы данных”"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println(number + number2);
+        System.out.println(number - number2);
+        System.out.println(number * number2);
+        double quotient = (double) number / number2;
+        System.out.println(quotient);
     }
 }
